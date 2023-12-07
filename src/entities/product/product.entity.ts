41543be0270020af6e57ab26b92d34223ea70408
amirhossein <you@example.com>
2023-12-07 14:31:01 +0330

    fix product and user and category bugs

--- src/entities/product/product.entity.ts
+++ src/entities/product/product.entity.ts
@@ -1,10 +1,30 @@
-import { Collection, ArangoDocument, BeforeSave } from 'nest-arango';
+import { Collection, ArangoDocument } from 'nest-arango';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Collection('Products')
 export class ProductEntity extends ArangoDocument {
+  @ApiProperty({ description: 'product pk', example: '1' })
+  product_id?: number;
+
   @ApiProperty({ description: 'نام محصول', example: 'موز' })
   product_name?: string;
+
+  @ApiProperty({ description: 'ایدی تامین کننده محصول', example: '2' })
+  supplier_id?: number;
+
+  @ApiProperty({
+    description: 'مقدار باقی مانده محصول در انبار',
+    example: '5 کیلو گرم',
+  })
+  balance?: string;
+
+  @ApiProperty({
+    description: 'آیدی دسته بندی ',
+    example: '[1, 2, 3]',
+  })
+  category_id?: string;
+
+  image_id?: number;
 
   @ApiProperty({
     description: 'توضیحات محصول',
@@ -15,39 +35,9 @@ export class ProductEntity extends ArangoDocument {
   @ApiProperty({ description: 'قیمت', example: '1000' })
   price?: number;
 
-  image_id?: number;
-
-  @ApiProperty({
-    description: 'آیدی دسته بندی ',
-    example: '[1, 2, 3]',
-  })
-  category_id?: string[];
-
-  @ApiProperty({ description: 'موجودی اولیه', example: '20' })
-  initial_balance?: number;
-  @ApiProperty({ description: 'موجودی فعلی', example: '10' })
-  current_balance?: number;
-  created_at?: Date;
-  updated_at?: Date;
   @ApiProperty({ description: 'تاریخ انقضا محصول', example: '2024/12/03' })
   expiry_date?: Date;
-  @ApiProperty({
-    description: 'نظرات',
-    example: '["این محصول برای فصل زمستان هست"]',
-  })
-  comments?: string[];
-  @ApiProperty({ description: 'کد محصول', example: 'GFD58LIBGDX2DS5V' })
-  product_code?: string;
-  @ApiProperty({ description: 'محصولات مرتبط', example: '[پرتقال ,ماهی]' })
-  related_products?: string[];
+
   @ApiProperty({ description: 'برند محصول', example: 'چی توز' })
   brand?: string;
-  @ApiProperty({ description: 'محصولات جایگزین', example: '[انار ,سیب]' })
-  alternative_products?: string[];
-
-  @BeforeSave()
-  beforeSave() {
-    this.created_at = new Date();
-    this.updated_at = new Date();
-  }
 }
