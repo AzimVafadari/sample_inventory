1cd46125d06d8d539dc06264be2143e9ebef8bc7
amirhossein <you@example.com>
2023-12-07 14:31:01 +0330

    fix product and user and category bugs

--- src/entities/category/category.entity.ts
+++ src/entities/category/category.entity.ts
@@ -5,20 +5,24 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CategoryEntity extends ArangoDocument {
   @ApiProperty({ description: 'category id', example: '1' })
   category_id?: string;
+
   @ApiProperty({
     description: 'category name',
     example: 'صیفی جات',
   })
   category_name?: string;
-  @ApiProperty({
-    description: 'category parent id',
-    example: '1',
-  })
-  parent_id?: string;
+
+  image_id: number;
+
   @ApiProperty({
     description: 'category description',
     example: 'این دسته بندی دارای میوه ها است',
   })
   description?: string;
-  image_id: number;
+
+  @ApiProperty({
+    description: 'category parent id',
+    example: '1',
+  })
+  parent_id?: string;
 }
