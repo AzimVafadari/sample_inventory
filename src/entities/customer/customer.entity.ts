15b3c4b5c7546e869443edff55afba1d88997007
Azim <azimvafadari@gmail.com>
2023-12-07 13:34:12 +0330

    Customer entity is created and it's properties are initialized.

---
+++ src/entities/customer/customer.entity.ts
@@ -0,0 +1,26 @@
+import { Collection, ArangoDocument } from 'nest-arango';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Collection('Reports')
+export class CustomerEntity extends ArangoDocument {
+  @ApiProperty({
+    description: 'آیدی مشتری',
+    example: '1',
+  })
+  customer_id?: string;
+  @ApiProperty({
+    description: 'نام مشتری',
+    example: 'علی حسینی',
+  })
+  name?: string;
+  @ApiProperty({
+    description: ' (0 برای فروش 1 برای خرید)آیدی سفارشات',
+    example: '["11", "02"]',
+  })
+  orders_id?: string[];
+  @ApiProperty({
+    description: 'آدرس',
+    example: 'یزد میدان امام علی',
+  })
+  address?: string;
+}
