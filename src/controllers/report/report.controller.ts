dad04d17257ff9bea650c0216f99626ab2b752d9
amirhossein <you@example.com>
2023-12-20 12:11:46 +0330

    remove and get all report of report controller are implemented

--- src/controllers/report/report.controller.ts
+++ src/controllers/report/report.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { ReportEntity } from 'src/entities/report/report.entity';
 import { ReportService } from 'src/services/report/report.service';
@@ -6,11 +6,28 @@ import { ReportService } from 'src/services/report/report.service';
 @Controller('report')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
+
   @Post()
   @ApiOperation({
     summary: 'ایجاد گزارش',
   })
   async createReport(@Body() report: ReportEntity) {
     return await this.reportService.create(report);
+  }
+
+  @Delete(':report_id')
+  @ApiOperation({
+    summary: 'حذف گزارش',
+  })
+  async removeReport(@Param('report_id') report_id: string) {
+    return await this.reportService.remove(report_id);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'یافتن همه گزارشات',
+  })
+  async getAllReports() {
+    return await this.reportService.findAll();
   }
 }
