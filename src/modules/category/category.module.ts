36388a1219609c823d7983c6b52a42851472425c
amirhossein <you@example.com>
2023-12-24 21:10:19 +0330

    all the errors are handled that means modularity of system is ended

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryController } from '../../controllers/category/category.controller';
 import { CategoryService } from '../../services/category/category.service';
 import { CategoryEntity } from '../../entities/category/category.entity';
+import { ArangoModule } from 'nest-arango';
 
 @Module({
-  imports: [CategoryEntity],
+  imports: [ArangoModule.forFeature([CategoryEntity])],
+  exports: [CategoryService],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '../../auth/constants';
 import { UserController } from '../../controllers/user/user.controller';
 import { UserService } from '../../services/user/user.service';
 import { UserEntity } from '../../entities/user/user.entity';
 import { ArangoModule } from 'nest-arango';
 
 @Module({
-  imports: [
-    ArangoModule.forFeature([
-      UserEntity,
-      // ProductEntity,
-      // CategoryEntity,
-      // ReportEntity,
-      // CustomerEntity,
-      // SupplierEntity,
-      // BuyOrderEntity,
-      // SaleOrderEntity,
-    ]),
-  ],
+  imports: [ArangoModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
