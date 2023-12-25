da85e8dda81f0ec8e09447b4a139ed7547a84a16
Azim <azimvafadari@gmail.com>
2023-12-25 18:45:42 +0330

    Error in product module is solved.

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,10 +3,12 @@ import { ProductController } from '../../controllers/product/product.controller'
 import { ProductService } from '../../services/product/product.service';
 import { ProductEntity } from '../../entities/product/product.entity';
 import { ArangoModule } from 'nest-arango';
+import { ReportEntity } from '../../entities/report/report.entity';
+import { ReportService } from '../../services/report/report.service';
 
 @Module({
-  imports: [ArangoModule.forFeature([ProductEntity])],
+  imports: [ArangoModule.forFeature([ProductEntity, ReportEntity])],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, ReportService],
 })
 export class ProductModule {}
