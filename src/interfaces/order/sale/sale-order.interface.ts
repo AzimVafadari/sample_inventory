a74585a633b273884cc3c3cf0484aada2b0ce223
amirhossein <you@example.com>
2024-01-28 09:32:30 +0330

    validation for saleorderChanges interface is implemented

--- src/interfaces/order/sale/sale-order.interface.ts
+++ src/interfaces/order/sale/sale-order.interface.ts
@@ -1,9 +1,31 @@
-export interface SaleOrderFilter {
-  product_id: string;
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class SaleOrderFilter {
+  @IsOptional()
+  @IsString()
+  product_id?: string;
+
+  @IsOptional()
+  @IsString()
   customer_id: string;
-  amount_from: number;
-  amount_to: number;
-  status: string;
+
+  @IsOptional()
+  @IsNumber()
+  amount_from?: number;
+
+  @IsOptional()
+  @IsNumber()
+  amount_to?: number;
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+
+  @IsOptional()
+  @IsDateString()
   date_from?: Date;
+
+  @IsOptional()
+  @IsDateString()
   date_to?: Date;
 }
